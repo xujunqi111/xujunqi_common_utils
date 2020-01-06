75f3dbf13174ebef07ce7a635a32628d5b7749a5
Dell <Dell@DESKTOP-K8BFU9N>
2020-01-06 20:01:04 +0800

    提交工具类

---
+++ src/test/java/com/xujunqi/common/utils/test/FileUtilTest.java
@@ -0,0 +1,15 @@
+package com.xujunqi.common.utils.test;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.xujunqi.common.utils.FileUtil;
+
+public class FileUtilTest {
+	@Test
+	public void test() {
+		List<String> readTextFileOfList = FileUtil.readTextFileOfList("C:\\Users\\Administrator\\Desktop\\pom.xml");
+		System.out.println(readTextFileOfList);
+	}
+}
